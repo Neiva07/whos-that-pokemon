67f0fb96de82c39b4b158c53f3b300f235126cd4
Neiva07 <lucasneiva07@gmail.com>
2019-11-25 13:58:42 -0300

    db config and main router

--- main.go
+++ main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+//App instance to build the app
+type App struct {
+	Router *mux.Router
+}
+
+//UseRouter Method to initilize all routers of the app
+func (app *App) UseRouter() {
+	app.Router = mux.NewRouter()
+
+	app.Router.HandleFunc("/", mainFunc).Methods("GET")
+
+}
+
+func main() {
+	app := App{}
+
+	app.UseRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, app.Router))
+
+}
+
+func mainFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	x := struct {
+		Data string
+	}{
+		Data: "Matheus Otário",
+	}
+	err := json.NewEncoder(w).Encode(x)
+	log.Println(x)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
---
+++ models/db.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+)
+
+//Database - struct holding the db itself and the methods attached to it
+type Database struct {
+	db *gorm.DB
+}
+
+//Initialize method to initialize the database
+func (DB *Database) Initialize() {
+
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	// dbName := os.Getenv("db_name")
+	// dbHost := os.Getenv("db_host")
+
+	dbURI := fmt.Sprintf("%s:%s@/?charset=utf8&parseTime=True&loc=Local", username, password)
+
+	connection, err := gorm.Open("mysql", dbURI)
+
+	if err != nil {
+		log.Print(err)
+	}
+	DB.db = connection
+	// DB.db.Debug().AutoMigrate
+}
+
+//GetDB exporting the dabaase to the rest of the application
+func (DB *Database) GetDB() *gorm.DB {
+	return DB.db
+}
+
+//DB declare the variable that could be exported
+var DB Database
+
+func init() {
+
+	err := godotenv.Load(os.ExpandEnv("$GOPATH/src/whos-that-pokemon/.env"))
+
+	if err != nil {
+		log.Print(err)
+	}
+	DB.Initialize()
+}
